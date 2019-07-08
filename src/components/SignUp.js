a99d8b0daf27bf8a633a4a3ebaf8f0bf1765636a
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-08 14:46:31 -0400

    created nonfunctioning login/signup components

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class SignUp extends React.Component {
+
+  render(){
+    return (
+      <div>
+        signing up
+      </div>
+    )
+  }
+}
+
+export default SignUp
