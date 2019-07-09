921d5d27f0f08a60831dfd799a0b33b1028204fe
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-09 16:41:04 -0400

    setup basic component, displays title

---
+++ src/components/Opportunities.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Opportunities extends React.Component {
+
+  render(){
+    return (
+      <div>
+        Opportunities
+      </div>
+    )
+  }
+}
+
+export default Opportunities
---
+++ src/components/Organizations.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Organizations extends React.Component {
+
+  render(){
+    return (
+      <div>
+        Organizations
+      </div>
+    )
+  }
+}
+
+export default Organizations
