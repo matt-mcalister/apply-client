174d6910742e206f419a08f82e29724e3a83403d
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-10 11:21:37 -0400

    authReducer now handles setuser and seterrors

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -9,7 +9,12 @@ const initialState = {
 
 const authReducer = (state = initialState, action) => {
   switch (action.type){
-
+    case actions.SET_USER:
+      return {...state, user: action.payload.user, token: action.payload.token}
+    case actions.SET_ERRORS:
+      return {...state, errors: action.payload}
+    case actions.CLEAR_ERRORS:
+      return {...state, errors: null}
     default:
     return state
 
