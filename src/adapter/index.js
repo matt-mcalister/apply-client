305a67827a4843e59ce098dabe5880202256a485
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-10 11:31:09 -0400

    setup adapter login action

--- src/adapter/index.js
+++ src/adapter/index.js
@@ -4,6 +4,10 @@ export const signUp = (user_info) => {
   return postRequest("/signup", user_info)
 }
 
+export const login = (user_info) => {
+  return postRequest("/login", user_info)
+}
+
 
 const postRequest = (route, body) => {
   return fetch(BASE_URL + route, {
