cf5a9631e19c7063c077c600c06774e983f23bff
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-10 11:51:54 -0400

    setup headers object for use when there is a token

--- src/adapter/constants.js
+++ src/adapter/constants.js
@@ -2,3 +2,8 @@ export const BASE_URL = "http://localhost:3001"
 export const HEADERS = {
   "Content-Type": "application/json"
 }
+
+export const AUTH_HEADERS = {
+  ...HEADERS,
+  "Authorization": localStorage.getItem("jwt-app.ly")
+}
